ec1ad55c208860ac8b064fb7afb26ddeeb9fb640
Christiana Vandermale <christianavdm@gmail.com>
2018-05-22 01:35:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+  end
+  
+  def kind 
+    if @a == @b && @b == @c
+      :equilateral 
+    elsif @a == @b || @b == @c || @c == @a 
+      :isosceles 
+    else 
+      :scalene
+    end
+  end
+  
 end
